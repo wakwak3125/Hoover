6b0250421493e303a45034735ee0c86635e57006
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2017-07-23 15:00:27 +0900

    Scaffold util class

---
+++ hoover-processor/src/main/kotlin/com/wakwak/hoover_processor/HooverUtils.kt
@@ -0,0 +1,15 @@
+package com.wakwak.hoover_processor
+
+/**
+ * Created by Ryo on 2017/07/21.
+ */
+object HooverUtils {
+
+    fun isRealmObject(): Boolean {
+        TODO("RealmObjectかどうかのチェックを行う")
+    }
+
+    fun hasRealmObjectField(): Boolean {
+        TODO("RealmObjectを持っているかどうかチェックする")
+    }
+}
